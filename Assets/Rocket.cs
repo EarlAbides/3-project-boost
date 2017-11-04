2a1b2f1a7d23099d4568f6a8aba595531c19fc91
Jeffrey Earl <jeffreyearl@hotmail.com>
2017-11-04 11:25:57 -0400

    Create ship with audio

---
+++ Assets/Rocket.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        HandleSpaceKey();
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back);
+        }
+    }
+
+    private void HandleSpaceKey()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+    }
+}
